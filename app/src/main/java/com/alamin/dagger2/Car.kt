3ef178d187477556c1c350e7fc858fe43acde0d5
Al-AminJoy <alaminislam3555@gmail.com>
2022-04-05 12:56:03 +0600

    Component

---
+++ app/src/main/java/com/alamin/dagger2/Car.kt
@@ -0,0 +1,19 @@
+package com.alamin.dagger2
+
+import android.util.Log
+import javax.inject.Inject
+
+private const val TAG = "Car"
+class Car {
+    lateinit var engine: Engine;
+    lateinit var wheels: Wheels;
+    @Inject
+    constructor(engine: Engine,wheels: Wheels){
+        this.engine = engine;
+        this.wheels = wheels;
+    }
+
+    fun startCar(){
+        Log.d("CAR_INFO", " Car Is Running")
+    }
+}
